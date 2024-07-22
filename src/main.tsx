bc8385f01934288bcda847c562f7665c0658c967
Ludvig <ludvig.fendert@appliedtechnology.se>
2024-07-22 10:40:44 +0200

    implemented tailwinds and clerk
    Co-authored-by: Simon Kendall <simon.a.kendall@gmail.com>
    Co-authored-by: Golda <goldamarymanackan@gmail.com>
    Co-authored-by: Mathangi Pushparajah <mathangi23@outlook.com>

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { ClerkProvider } from "@clerk/clerk-react";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl={"/"}>
     <App />
-  </React.StrictMode>,
-)
+    </ClerkProvider>
+  </React.StrictMode>
+);
