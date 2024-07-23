1580dceb5b73b2f6f13e995fe6bf8a5513bde7f3
Ludvig <ludvig.fendert@appliedtechnology.se>
2024-07-23 12:08:14 +0200

    updated endpoint approvale now works

--- src/ApiQueries/useRequests.ts
+++ src/ApiQueries/useRequests.ts
@@ -1,36 +1,24 @@
 import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
 
-const apiHost = 'http://localhost:8080'; // Hardcoding deal with it potential github stalker
+const apiHost = 'http://localhost:8080'; // Hardcoding the API host for local development
 const requestsEndpoint = '/requests';
 
 const getRequests = async () => {
   const url = `${apiHost}${requestsEndpoint}`;
-  const response = await axios.get(url);
-  return response.data;
+  try {
+    const response = await axios.get(url);
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching requests:', error);
+    throw error;
+  }
 };
 
 const useRequests = () => {
-  const queryFunction =
-    import.meta.env.MODE === 'development'
-      ? async () => {
-        return [
-          {
-            id: 1,
-            user: { id: 1, name: 'Alice' },
-            products: [
-              { id: 1, product: { id: 1, name: 'Product A' }, quantity: 2 },
-              { id: 2, product: { id: 2, name: 'Product B' }, quantity: 5 },
-            ],
-            isApproved: false, //just for checking
-          },
-        ];
-      }
-      : getRequests;
-
   return useQuery({
     queryKey: ['requests'],
-    queryFn: queryFunction,
+    queryFn: getRequests,
   });
 };
 
