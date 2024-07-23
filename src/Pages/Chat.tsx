c61c57c6e5573b5874c6d51ea062314a5ff090db
Simon Kendall <simon.a.kendall@gmail.com>
2024-07-23 16:07:58 +0200

    add rudimentary websocket connection

--- src/Pages/Chat.tsx
+++ src/Pages/Chat.tsx
@@ -1,3 +1,13 @@
+import { useEffect } from "react";
+
 export const Chat = () => {
+  useEffect(() => {
+    const socket = new WebSocket("ws://localhost:8080/chat");
+    socket.addEventListener("open", (event) => {
+      console.log(event);
+      socket.send("Connection established! Hello!");
+    });
+  }, []);
+
   return <p>Chat</p>;
 };
