2596d1904f66626250b083810dc1075c575e4da3
Simon Kendall <simon.a.kendall@gmail.com>
2024-07-25 15:19:47 +0200

    configure tailwind and mui to play nice

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
 import { RouterProvider, createRouter } from "@tanstack/react-router";
 import { routeTree } from "./routes/route";
-import { ThemeProvider } from "@emotion/react";
-import { createTheme } from "@mui/material";
 
 const router = createRouter({ routeTree });
-
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: "#113264",
-    },
-  },
-});
 
 function App() {
   return (
     <>
-      <ThemeProvider theme={theme}>
-        <RouterProvider router={router} />
-      </ThemeProvider>
+      <RouterProvider router={router} />
     </>
   );
 }
