707f80e87a07c5f1e2c11e7e8500b25fafb43cf4
Mathangi Pushparajah <mathangi23@outlook.com>
2024-07-30 12:17:30 +0200

    fixed footer so it's consistent and stay at the bottom regardless of page content

--- src/routes/NavBar.tsx
+++ src/routes/NavBar.tsx
@@ -65,7 +65,7 @@ export const NavBar = () => {
           </Drawer>
         </SignedIn>
 
-        <Box component="main" className="w-full overflow-hidden p-20 h-screen">
+        <Box component="main" className="w-full overflow-hidden p-20 h-full">
           <Toolbar />
           <Outlet />
         </Box>
