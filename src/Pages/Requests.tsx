9ac1a9d10be9605e97ce88414e916e9ca3bc4d1e
Simon Kendall <simon.a.kendall@gmail.com>
2024-07-30 10:00:05 +0200

    clean pages

--- src/Pages/Requests.tsx
+++ src/Pages/Requests.tsx
@@ -7,6 +7,7 @@ import "react-toastify/dist/ReactToastify.css";
 import RequestsForm from "../Components/RequestsForm";
 import IsAdmin from "../Components/IsAdmin";
 import SearchBar from "../Components/SearchBar";
+import { Request } from "../utils/types";
 
 export const Requests: React.FC = () => {
   const { data: requests, error, isLoading, refetch } = useRequests();
@@ -40,10 +41,10 @@ export const Requests: React.FC = () => {
 
   console.log("Fetched requests:", requests); // Log the fetched data
 
-  const filteredRequests = requests?.filter((request) => {
+  const filteredRequests = requests?.filter((request: Request) => {
     const userName = request.user?.name.toLowerCase() || "";
-    const products = request.products.map((product) =>
-      product.product?.name.toLowerCase() || ""
+    const products = request.products.map(
+      (product) => product.product?.name.toLowerCase() || ""
     );
     return (
       userName.includes(searchTerm.toLowerCase()) ||
@@ -61,7 +62,10 @@ export const Requests: React.FC = () => {
         >
           Toggle Show Archived
         </button>
-        <SearchBar searchTerm={searchTerm} handleSearchChange={handleSearchChange} />
+        <SearchBar
+          searchTerm={searchTerm}
+          handleSearchChange={handleSearchChange}
+        />
         <h1 className="text-3xl font-bold mb-4">User Requests</h1>
         {filteredRequests?.length === 0 ? (
           <p>No requests at the moment.</p>
@@ -80,10 +84,14 @@ export const Requests: React.FC = () => {
               </thead>
               <tbody>
                 {filteredRequests
-                  ?.filter((request) => showArchived || !request.approved)
-                  .map((request) => (
+                  ?.filter(
+                    (request: Request) => showArchived || !request.approved
+                  )
+                  .map((request: Request) => (
                     <tr key={request.id} className="hover:bg-gray-100">
-                      <td className="px-4 py-2 border-b text-center">{request.id}</td>
+                      <td className="px-4 py-2 border-b text-center">
+                        {request.id}
+                      </td>
                       <td className="px-4 py-2 border-b text-center">
                         {request.user?.name || "Unknown"}
                       </td>
@@ -91,7 +99,8 @@ export const Requests: React.FC = () => {
                         <ul>
                           {request.products.map((product) => (
                             <li key={product.id}>
-                              {product.product?.name || "Unknown"} (Quantity: {product.quantity})
+                              {product.product?.name || "Unknown"} (Quantity:{" "}
+                              {product.quantity})
                             </li>
                           ))}
                         </ul>
@@ -100,10 +109,12 @@ export const Requests: React.FC = () => {
                         <td className="px-4 py-2 border-b text-center">
                           {!request.approved && (
                             <button
-                              onClick={() => toggleApproval(request.id, request.isApproved)}
+                              onClick={() =>
+                                toggleApproval(request.id, request.approved)
+                              }
                               className="bg-blue-500 text-white px-3 py-1 rounded hover:bg-blue-900"
                             >
-                              {request.isApproved ? <FaTimes /> : <FaCheck />}
+                              {request.approved ? <FaTimes /> : <FaCheck />}
                             </button>
                           )}
                         </td>
@@ -115,7 +126,7 @@ export const Requests: React.FC = () => {
           </div>
         )}
       </div>
-      <RequestsForm refetch={refetch} />
+      <RequestsForm />
     </>
   );
 };
