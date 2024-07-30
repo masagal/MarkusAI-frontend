c04d5a1362efab168e343d5fd2ef300ed2e68f53
Simon Kendall <simon.a.kendall@gmail.com>
2024-07-30 12:13:34 +0200

    make inventory cards prettier

--- src/Components/InventoryCard.tsx
+++ src/Components/InventoryCard.tsx
@@ -1,5 +1,7 @@
 import { Card, CardContent, CardMedia, Typography } from "@mui/material";
 import { Product } from "../utils/types";
+import { Place } from "@mui/icons-material";
+import GrainIcon from "@mui/icons-material/Grain";
 
 export const InventoryCard = ({ product }: { product: Product }) => {
   return (
@@ -12,9 +14,23 @@ export const InventoryCard = ({ product }: { product: Product }) => {
         <Typography variant="h6">
           <p>{product.name}</p>
         </Typography>
-        <Typography>
-          <p>Quantity: {product.quantity}</p>
-          <p>Location: {product.location}</p>
+        <Typography variant="body1" className="font-medium">
+          <div className="flex justify-items-stretch mt-20">
+            <div className="flex text-center justify-items-center align-items-between flex-col basis-1/2">
+              <div>
+                <GrainIcon />
+              </div>
+              in stock
+              <br />
+              {product.quantity}
+            </div>
+            <div className="flex justify-items-center text-center align-items-between flex-col basis-1/2">
+              <div>
+                <Place />
+              </div>
+              {product.location}
+            </div>
+          </div>
         </Typography>
       </CardContent>
     </Card>
