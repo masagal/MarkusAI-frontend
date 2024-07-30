e176ade4f00102a980193ae3c3dde69669baa74f
Mathangi Pushparajah <mathangi23@outlook.com>
2024-07-30 13:24:01 +0200

    fix footer

--- src/routes/NavBar.tsx
+++ src/routes/NavBar.tsx
@@ -65,7 +65,7 @@ export const NavBar = () => {
           </Drawer>
         </SignedIn>
 
-        <Box component="main" className="w-full overflow-hidden p-20 h-full">
+        <Box component="main" className="w-full overflow-hidden p-20 h-screen">
           <Toolbar />
           <Outlet />
         </Box>
