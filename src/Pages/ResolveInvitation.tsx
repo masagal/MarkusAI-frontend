c3f7cb5596efa4e89f3d63d26888487c369c7147
Simon Kendall <simon.a.kendall@gmail.com>
2024-07-31 15:14:22 +0200

    add navigation and spinners
    Co-authored-by: Ludvig Fendert <ludvig.fendert@appliedtechnology.se>
    Co-authored-by: Mathangi Pushparajah <mathangi23@outlook.com>

--- src/Pages/ResolveInvitation.tsx
+++ src/Pages/ResolveInvitation.tsx
@@ -1,7 +1,8 @@
 import { SignInButton, SignedOut, SignedIn } from "@clerk/clerk-react";
 import { useAuth } from "@clerk/clerk-react";
 import { useInvitation } from "../ApiQueries/useUserData";
-import { useSearch } from "@tanstack/react-router";
+import { Navigate, useNavigate, useSearch } from "@tanstack/react-router";
+import { CircularProgress, Typography } from "@mui/material";
 
 const ResolveInvitation = () => {
   const auth = useAuth();
@@ -13,20 +14,37 @@ const ResolveInvitation = () => {
     inviteMutation.mutate(inviteToken);
   }
   if (inviteMutation.isSuccess) {
-    return "yeah done";
+    return <Navigate to="/"></Navigate>;
   }
   if (!inviteToken) {
-    return "Dont";
+    return (
+      <Typography>
+        This URL is missing the invite token and is thus invalid.
+      </Typography>
+    );
   }
   return (
     <>
-      <SignedOut>
-        <p>Please sign in to resolve this invite.</p>
-        <SignInButton>Sign in</SignInButton>
-      </SignedOut>
-      <SignedIn>
-        <p>Processing...</p>
-      </SignedIn>
+      <Typography>
+        <SignedOut>
+          <p>Please sign in to resolve this invite.</p>
+          <SignInButton>Sign in</SignInButton>
+        </SignedOut>
+        <SignedIn>
+          {inviteMutation.isPending && (
+            <>
+              <p>Processing...</p>
+              <CircularProgress />
+            </>
+          )}
+          {inviteMutation.isError && (
+            <>
+              <p>Something went wrong when resolving this invite.</p>
+              <p>Perhaps you have already used it?</p>
+            </>
+          )}
+        </SignedIn>
+      </Typography>
     </>
   );
 };
